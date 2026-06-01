f77c646f27b895c630ef15254565cba16cbd7958
lilsneq <bilmork2407@gmail.com>
2026-06-02 00:08:16 +0300

    упростил main файл

--- main.py
+++ main.py
@@ -2,8 +2,22 @@
 Discord Bot для системы заявок в семью
 Модульная структура проекта
 """
-
+import logging
 import discord
+import sys
+import asyncio
+
+logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s - [%(levelname)s] - %(message)s',
+        datefmt='%m/%d/%Y %I:%M:%S %p',
+        handlers=[
+            logging.StreamHandler(sys.stdout),
+        ]
+    )
+
+logging.getLogger("discord").setLevel(logging.WARNING)
+
 from discord.ext import commands
 from config.settings import TOKEN
 from commands.static import setup_static_commands
@@ -11,58 +25,49 @@ from events.bot_events import setup_bot_events
 from events.server_events import setup_server_events
 from cogs.cogi import new_capt_command
 from events.subscription_check import SubscriptionTasks
-import ssl
-import os
-
-# Исправление сертификатов
-if not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
-    ssl._create_default_https_context = ssl._create_unverified_context
 
 
-class MyBot(commands.Bot):
-    def __init__(self):
-        intents = discord.Intents.default()
-        intents.message_content = True
-        intents.members = True
-        intents.guilds = True
-        super().__init__(
-            command_prefix='l.',
-            intents=intents,
-            status=discord.Status.idle,
-            activity=discord.Activity(type=discord.ActivityType.watching, name='majestic-rp.ru'),
-            help_command=None
-        )
+# СКРИПТ
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+intents.guilds = True
+
+bot = commands.Bot(
+    command_prefix='l.',
+    intents=intents,
+    status=discord.Status.idle,
+    activity=discord.Activity(type=discord.ActivityType.watching, name='majestic-rp.ru'),
+    help_command=None
+)
 
 
-    async def setup_hook(self):
-        try:
-            await self.add_cog(SubscriptionTasks(self))
-            print("Задача проверки подписок запущена")
-        except Exception as e:
-            print(f'Ошибка проверки подписок {e}')
-
-        try:
-            self.tree.add_command(new_capt_command)
-
-            setup_static_commands(self)
-            setup_bot_events(self)
-            setup_server_events(self)
-
-            print("✅ Все команды и события зарегистрированы")
-        except Exception as e:
-            print(f'Ошибка запуска команд {e}')
-
-def main():
+async def main():
     if not TOKEN:
-        print('❌ Ошибка: TOKEN не найден!')
+        logging.warning('❌ Ошибка: TOKEN не найден!')
         return
 
-    bot = MyBot()
+    logging.info('🚀 Запуск бота...')
 
+    try:
+        await bot.add_cog(SubscriptionTasks(bot))
+        bot.tree.add_command(new_capt_command)
+        setup_static_commands(bot)
+        setup_bot_events(bot)
+        setup_server_events(bot)
+        logging.info("✅ Все команды и события зарегистрированы")
 
-    print('🚀 Запуск бота...')
-    bot.run(TOKEN)
+    except Exception:
+        logging.warning('ОШИБКА ЗАПУСКА БОТА', exc_info=True)
+        return
+
+    logging.info("✅ БОТ В РАБОТЕ.")
+
+    async with bot:
+        await bot.start(TOKEN)
+
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
