73ada779bbc54d069da160437a95ef408cf12bd0
lilsneq <bilmork2407@gmail.com>
2026-06-01 20:19:01 +0300

    добавил кнопки отправки заявки для роли

---
+++ models/application_rating_system.py
@@ -0,0 +1,166 @@
+# МОДАЛЬНОЕ ОКНО ДЛЯ ЗАЯВКИ НА ПОВЫШЕНИЕ
+import logging
+
+# ИМПОРТЫ
+
+import discord
+from discord.app_commands import guilds
+
+from config.settings import RATING_CHANNEL_ID, ADMIN_RATING_CHANNEL_ID
+
+
+
+# СКРИПТ
+
+
+class RatingDrop(discord.ui.Select):
+    def __init__(self):
+        options = [
+            discord.SelectOption(label="ACADEMY", value="1236152539968438312:1", description="Подать заявку на Номер 1",
+                                 emoji="🟢"),
+            discord.SelectOption(label="Recruit", value="1482962712844828713:2", description="Подать заявку на Номер 2",
+                                 emoji="🔵"),
+            discord.SelectOption(label="Sora", value="1482962888762069192:3", description="Подать заявку на Номер 3",
+                                 emoji="⚫"),
+            discord.SelectOption(label="Main Sora", value="1508432058865684671:4",
+                                 description="Подать заявку на Номер 4", emoji="🔴"),
+            discord.SelectOption(label="Chif rec", value="1509926246505779270:5",
+                                 description="Подать заявку на Номер 5", emoji="🟢"),
+            discord.SelectOption(label="High", value="1487937197142179880:6", description="Подать заявку на Номер 6",
+                                 emoji="🔵"),
+            discord.SelectOption(label="Dep", value="1487937119853744358:7", description="Подать заявку на Номер 7",
+                                 emoji="🔴"),
+        ]
+        super().__init__(
+            placeholder="Выберите желаемый номер для повышения...",
+            min_values=1,
+            max_values=1,
+            options=options,
+            custom_id="rating_system_dropdown"
+        )
+
+    async def callback(self, interaction: discord.Interaction):
+        role_id_str, number = self.values[0].split(":")
+        role_id = int(role_id_str)
+
+        await self.view.assign_role(interaction, role_id=role_id, number=number)
+
+
+class RatingSystemApplication(discord.ui.View):
+    """ОКНО НА ПОВЫШЕНИЕ"""
+    def __init__(self):
+        super().__init__(timeout=None)
+        self.add_item(RatingDrop())
+
+    async def assign_role(self, interaction: discord.Interaction, role_id: int, number: str):
+        guild = interaction.guild
+        role = guild.get_role(role_id)
+
+        if not role:
+            await interaction.response.send_message(
+                f'Ошибка: Роль не найдена на сервере',
+                ephemeral=True,
+            )
+            return
+        if role in interaction.user.roles:
+            await interaction.response.send_message(
+                f'У вас уже есть роль {role.name}',
+                ephemeral=True,
+            )
+            return
+
+        admin_ch = guild.get_channel(ADMIN_RATING_CHANNEL_ID)
+        if admin_ch:
+            admin_embed = discord.Embed(
+                title='Заявка на повышение',
+                color=discord.Color.green()
+            )
+            admin_embed.add_field(name="Пользователь", value=interaction.user.mention, inline=True)
+            admin_embed.add_field(name="Желаемый номер", value=f"Номер {number}", inline=True)
+            admin_embed.add_field(name="Запрашиваемая роль", value=role.mention, inline=False)
+            admin_embed.set_footer(text=f"ID пользователя: {interaction.user.id}")
+
+
+
+            await admin_ch.send(embed=admin_embed, view=AdminAcceptRatingSystem())
+
+            await interaction.response.send_message(
+                f'Ваша заявка успешно отправлена',
+                ephemeral=True,
+            )
+
+        else:
+            logging.error('Канал рассмотрения заявок не найдена')
+            return
+
+
+class AdminAcceptRatingSystem(discord.ui.View):
+    """ПАНЕЛЬ ОДОБРИТЬ/ОТКЛОНИТЬ У АДМИНА"""
+    def __init__(self):
+        super().__init__(timeout=None)
+
+
+    @discord.ui.button(label='Одобрить', style=discord.ButtonStyle.green, custom_id="admin_aprove")
+    async def button_admin_aprove(self, interaction: discord.Interaction, button: discord.ui.Button):
+        guild = interaction.guild
+
+        if not interaction.message.embeds:
+            await interaction.response.send_message("Пользователь или роль не найдены на сервере.", ephemeral=True)
+            return
+        embed = interaction.message.embeds[0]
+
+        try:
+            footer_text = embed.footer.text
+
+            parts = footer_text.split("|")
+            user_id = int(parts[0].split(": ")[1].strip())
+            role_id = int(parts[1].split(": ")[1].strip())
+
+        except Exception:
+            logging.error(f"Не удалось распарсить эмбед")
+            await interaction.response.send_message(
+                "Не удалось автоматически считать ID пользователя или роли из этой карточки.", ephemeral=True)
+            return
+
+        member = guild.get_member(user_id)
+        role = guild.get_role(role_id)
+
+        if not member or not role:
+            await interaction.response.send_message(" Пользователь или роль больше не найдены на сервере.",
+                        ephemeral=True)
+            return
+
+        try:
+            await member.add_roles(role)
+
+            embed.color = discord.Color.brand_green()
+            embed.title = f"Заявка одобрена администратором {interaction.user.display_name}"
+
+            await interaction.message.edit(embed=embed, view=None)
+            await interaction.response.defer()
+
+        except Exception:
+            logging.error('Ошибка одобрения роли')
+
+
+    @discord.ui.button(label="Отклонить", style=discord.ButtonStyle.red, custom_id="admin_deny")
+    async def deny(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if not interaction.message.embeds:
+            return
+        embed = interaction.message.embeds[0]
+        try:
+            user_id = int(embed.footer.text.split(": ")[1])
+            member = interaction.guild.get_member(user_id)
+        except Exception:
+            logging.error('ОШИБКА КНОПКИ ОТКЛОНИТЬ')
+
+        embed.color = discord.Color.red()
+        embed.title = f"Заявка отклонена администратором {interaction.user.display_name}"
+
+        await interaction.message.edit(embed=embed, view=None)
+        await interaction.response.defer()
+
+
+
+
+
